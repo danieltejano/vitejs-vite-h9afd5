43561bf72361134c313d268be2900d88435f29e1
danieltejano <53635919+danieltejano@users.noreply.github.com>
2023-07-18 09:17:47 +0800

    Initial commit

---
+++ src/components/Comp.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import type { MyType } from './Comp';
+
+defineProps<{
+  state: MyType;
+}>();
+</script>
+
+<template>
+  <h1>{{ state.hello }}</h1>
+</template>
---
+++ src/system.d.ts
@@ -0,0 +1,24 @@
+import { Ref } from "vue";
+
+interface SearchList {
+  limit : number,
+  offset : number,
+  term: string,
+  orderBy?: string,
+  order?: 'asc'|'desc',
+  filters: object
+}
+
+
+interface Crudable{
+  async create() : Ref<T>
+  async list(parameters: SearchList): Ref<T>[]
+  async show(token: string): Ref<T>
+  async update(token: string, payload: object): Ref<T>
+  async destroy?(token:string): boolean
+}
+
+class Model implements Crudable{
+  protected aliasName = this.constructor.name
+  
+}
